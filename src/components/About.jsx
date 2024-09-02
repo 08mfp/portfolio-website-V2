8d7a732ef371d1832e792fea85a9e7d9d88c4e35
08mfp <mohamedfaridpatel@gmail.com>
2024-09-02 15:08:43 +0200

    added buttons

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -35,6 +35,48 @@ const About = () => {
         I have created this website to showcase some of my skills, projects and experiences.
         </motion.p>
 
+      <div className="w-full flex row sm:flex-row gap-4 mt-6">
+      <a
+        href="https://github.com/08mfp"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex justify-center items-center text-white bg-[#915EFF] px-4 py-2 rounded-full shadow-md hover:bg-purple-700 transition-colors duration-300 w-56 sm:w-auto"
+      >
+        <img
+          src="https://www.svgrepo.com/show/312259/github.svg"
+          alt="GitHub Logo"
+          className="w-5 h-5 mr-2"
+        />
+        GitHub
+      </a>
+      <a
+        href="https://www.linkedin.com/in/mohamedfaridpatel/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex justify-center items-center text-white bg-[#915EFF] px-4 py-2 rounded-full shadow-md hover:bg-purple-700 transition-colors duration-300 w-56 sm:w-auto"
+      >
+        <img
+          src="https://uxwing.com/wp-content/themes/uxwing/download/brands-and-social-media/linkedin-app-white-icon.png"
+          alt="LinkedIn Logo"
+          className="w-5 h-5 mr-2"
+        />
+        LinkedIn
+      </a>
+      <a
+        href="https://react-portfolio-tau-ten-62.vercel.app/"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="flex justify-center items-center text-white bg-[#915EFF] px-4 py-2 rounded-full shadow-md hover:bg-purple-700 transition-colors duration-300 w-56 sm:w-auto"
+      >
+        <img
+          src="https://www.freeiconspng.com/thumbs/website-icon/website-icon-11.png"
+          alt="Web Logo"
+          className="w-5 h-5 mr-2"
+        />
+        Old Website
+      </a>
+    </div>
+
       <div className='mt-20 flex flex-wrap gap-10'>
         {services.map((service, index) => (
           <ServiceCard key={service.title} index={index} {...service} />
